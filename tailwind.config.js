6b91635d8af685aa1692d219291a9a125e5d707e
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2025-01-01 10:11:22 +0530

    Install React Router Dom

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ export default {
       fontFamily: {
         sans: ["Poppins", "sans-serif"],
       },
+      colors: {
+        "main-color": "#0485cf",
+        blue: "#0000ff",
+        "blue-dark": "#18293c",
+        orange: "#ffa500",
+        "green-yellow": "#cddc39",
+        "pink-light": "#efa2b4",
+        "cyan-light": "#aef1ee",
+        white: "#ffffff",
+        "white-alpha-40": "rgba(255, 255, 255, 0.40)",
+        "white-alpha-25": "rgba(255, 255, 255, 0.25)",
+      },
     },
   },
   plugins: [],
